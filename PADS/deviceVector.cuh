a456d63408a73de4cfcaa47bf53a70b1280b4e3a
hakgagik <xddarkgalegh@aim.com>
2014-12-12 22:00:28 -0500

    Updated devicevector

--- PADS/deviceVector.cuh
+++ PADS/deviceVector.cuh
@@ -33,3 +33,60 @@ public:
 private:
 	double* v;
 };
+
+#define DVEC_BOUNDS_CHECK
+#ifdef DVEC_BOUNDS_CHECK
+#include <typeinfo>
+#endif
+
+__inline__ __device__ dvec::dvec(double *a, int nElem){
+	v = new double[nElem];
+	for (int i = 0; i < nElem; i++){
+		v[i] = a[i];
+	}
+	n_elem = nElem;
+}
+
+__device__ dvec::dvec(){
+	delete v;
+}
+
+__inline__ __device__ double dvec::operator[](const int i){
+#ifdef DVEC_BOUNDS_CHECK
+	if (i < 0 || i >= n_elem) { return 1E300; }
+#endif
+	return v[i];
+}
+
+__inline__ __device__ void dvec::operator+=(dvec v1){
+#ifdef DVEC_BOUNDS_CHECK
+	if (v1.n_elem != n_elem) { return; }
+#endif
+	for (int i = 0; i < n_elem; i++){
+		v[i] += v1.v[i];
+	}
+}
+__inline__ __device__ void dvec::operator-=(dvec v1){
+#ifdef DVEC_BOUNDS_CHECK
+	if (v1.n_elem != n_elem) { return; }
+#endif
+	for (int i = 0; i < n_elem; i++){
+		v[i] -= v1.v[i];
+	}
+}
+//__inline__ __device__ void dvec::operator*=(dvec v1){}
+//__inline__ __device__ void dvec::operator/=(dvec v1){}
+//__inline__ __device__ void dvec::operator+=(double s1){}
+//__inline__ __device__ void dvec::operator-=(double s1){}
+//__inline__ __device__ void dvec::operator*=(double s1){}
+//__inline__ __device__ void dvec::operator/=(double s1){}
+//__inline__ __device__ void dvec::operator++(){}
+//__inline__ __device__ void dvec::operator--(){}
+//
+//__inline__ __device__ double dot(dvec v1){}
+//__inline__ __device__ double cross(dvec v1){}
+//__inline__ __device__ double norm(){}
+//__inline__ __device__ double sum(){}
+
+//__device__ dvec clone();
+//__device__ double* toArray();
