49589175a50197774611699e2fd17b3268f42b50
hakgagik <xddarkgalegh@aim.com>
2014-12-15 09:48:40 -0500

    Corrected all of git's screwups

--- PADS/mainIterator.cu
+++ PADS/mainIterator.cu
@@ -29,6 +29,11 @@
 #define cy -.00003859
 #define cz 1.09983
 
+// Simple integer power function for the LJ potential.
+__inline__ __device__ double pown(double a, int n){
+
+}
+
 // Each thread block contains nBeads threads. There are nMols thread blocks. Therefore, each molecule is its own thread block.
 // Each molecule computes its own centroid and puts it into dcentroids
 // This is done via a parallel reduction algorithm that calculates a sum of n elements in O(log(n)) time.
@@ -210,19 +215,19 @@ __global__ void MDStep(double *xGlobal, double *yGlobal, double *zGlobal, int *v
 
 	// Next, the theta term. A bit more complicated. Each molecule recieve a contribution from the angle behind it, the angle ahead of it, and the angle that has it as the origin.
 	if (j < (b - 2)) {
-		factor = 2 * k_th * (th_0 - PI + theta[j + 1]) / r[j];
+		factor = 2 * k_th * (th_0 - PI + theta[j + 1]) / r[j] / sin(theta[j + 1]);
 		Fx += factor * (dxpp / r[j + 1] + cos(theta[j + 1]) * dxp / r[j]);
 		Fy += factor * (dypp / r[j + 1] + cos(theta[j + 1]) * dyp / r[j]);
 		Fz += factor * (dzpp / r[j + 1] + cos(theta[j + 1]) * dzp / r[j]);
 	}
 	if (j > 1) {
-		factor = 2 * k_th * (th_0 - theta[j - 1] / r[j - 1]);
+		factor = 2 * k_th * (th_0 - PI + theta[j - 1] / r[j - 1]) / sin(theta[j - 1]);
 		Fx += factor * ((x[j - 2] - x[j - 1]) / r[j - 2] + cos(theta[j - 1]) * dxm);
 		Fy += factor * ((y[j - 2] - y[j - 1]) / r[j - 2] + cos(theta[j - 1]) * dym);
 		Fz += factor * ((z[j - 2] - z[j - 1]) / r[j - 2] + cos(theta[j - 1]) * dzm);
 	}
 	if (j > 0 && j < (b - 1)){
-		factor = 2 * k_th * (th_0 - theta[j]);
+		factor = 2 * k_th * (th_0 - PI + theta[j]) / sin(theta[j]);
 		Fx -= factor * (dxm / r[j - 1] - cos(theta[j]) * dxp / r[j]) / r[j]
 			+ (dxp / r[j] - cos(theta[j]) * dxm / r[j - 1]) / r[j - 1];
 	}
@@ -252,6 +257,8 @@ __global__ void MDStep(double *xGlobal, double *yGlobal, double *zGlobal, int *v
 		Fy += factor * ((y[j - 3] - y[j - 2]) / r[j - 3] + cos(phi[j - 2]) * dym / r[j - 1]) / r[j - 1];
 		Fz += factor * ((z[j - 3] - z[j - 2]) / r[j - 3] + cos(phi[j - 2]) * dzm / r[j - 1]) / r[j - 1];
 	}
+	
+	// Finally, Van der Walls
 
 }
 
